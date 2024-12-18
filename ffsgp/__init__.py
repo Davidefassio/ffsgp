bad187e20573e899e7937b370dcf868e3673691d
Davidefassio <davidefassio7@gmail.com>
2024-12-18 14:09:01 +0100

    add mutation

--- ffsgp/__init__.py
+++ ffsgp/__init__.py
@@ -6,7 +6,7 @@
 from .operators import add, sub, mul, div, pow, log, sin, cos, abs
 from .variables import *
 from .tree import *
-from .genetic_operators import crossover_subtree, mutation_single_point, mutation_hoist, mutation_subtree, create_full, create_grow
+from .genetic_operators import crossover_subtree, mutation_single_point, mutation_constant, mutation_hoist, mutation_subtree, create_full, create_grow
 from .initial_population import init_pop_full, init_pop_grow, init_pop_half, init_pop_ramp
 from .metrics import *
 from .parent_selection import *
